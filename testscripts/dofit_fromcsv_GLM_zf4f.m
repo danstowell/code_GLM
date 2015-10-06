3a34c63a08180ac128124847dd34773161719afb
Dan Stowell <danstowell@users.sourceforge.net>
2015-10-06 13:30:51 +0100

    k as a variable. (can't cope with k!=4 yet tho)

--- testscripts/dofit_fromcsv_GLM_zf4f.m
+++ testscripts/dofit_fromcsv_GLM_zf4f.m
@@ -5,18 +5,16 @@ function [numcalls, peakpos, peakval, neglogli] = dofit_fromcsv_GLM_zf4f(csvpath
 % also does a plot and writes it to a file in the folder named by 'plotpath'. if 'plotpath' is '' or 0 it DOESN'T plot. to plot in cwd use '.'
 % 'csvoutpath' parameter is analogous, and is about writing CSV data out to file
 
-% TODO add 'csvoutpath' parameter, treated analogously to plotpath
-
-% TODO adapt the code so that it correctly adapts to varying k
-
 global RefreshRate;
 RefreshRate = 1;  % the "RefreshRate" is the samplerate of the stimulus (in Hz). I don't currently use stimulus so I set it to 1. Below, "DTsim" sets the time-resultion used in the model.
 
 plotcols = {'r', 'b', 'g', 'm'};
 
-numcalls = zeros(4,1);
-peakpos = zeros(4);
-peakval = zeros(4);
+k = 4; % TODO adapt the code so that it correctly adapts to varying k
+
+numcalls = zeros(k,1);
+peakpos = zeros(k);
+peakval = zeros(k);
 
 printf('dofit_fromcsv_GLM_zf4f(%s, %s, %s, %i, %i, %s, %s)\n', csvpath, runlabel, mat2str(indexmapper), startsecs, endsecs, plotpath, csvoutpath);
 
@@ -27,8 +25,8 @@ events = csvread(csvpath);
 events = events((events(:,1) >= startsecs) & (events(:,1) <= endsecs), [1,3]);
 %size(events)
 
-tsp = cell(1,4);
-for whichn=1:4
+tsp = cell(1,k);
+for whichn=1:k
 	matchid = indexmapper(whichn)-1;
 	tsp{whichn} = (events(events(:,2)==matchid, 1) - startsecs) * RefreshRate;   % subtracting startsecs, and converting to units of RefreshRate
 	numcalls(whichn) = size(tsp{whichn}, 1);
@@ -43,11 +41,11 @@ fflush(stdout); % NB octave-only
 DTsim = .005; % Bin size for simulating model & computing likelihood (in units of stimulus frames)
 nkt = 40;    % Number of time bins in filter;
 ttk = [-nkt+1:0]';
-ggsimsolo = cell(4,1);
-for whichn = 1:4
+ggsimsolo = cell(k,1);
+for whichn = 1:k
 	ggsimsolo{whichn} = makeSimStruct_GLM(nkt,DTsim);  % Create GLM struct with default params
 end;
-ggsim = makeSimStruct_GLMcpl(ggsimsolo{1},ggsimsolo{2},ggsimsolo{3},ggsimsolo{4});
+ggsim = makeSimStruct_GLMcpl(ggsimsolo{1},ggsimsolo{2},ggsimsolo{3},ggsimsolo{4});  % TODO how can this deal with varying k?
 
 
 %% 3. Set up the "stimulus" appropriately (here it's zeros)
@@ -61,18 +59,18 @@ Stim = zeros(slen,swid);
 
 % -------------- Compute STAs------------
 nsp = length(tsp{1});
-stas = cell(4,1);
-for whichn = 1:4
+stas = cell(k,1);
+for whichn = 1:k
 	sta0 = simpleSTC(Stim,tsp{whichn},nkt);
 	stas{whichn} = reshape(sta0,nkt,[]); 
 end;
 
 
 %% 4. Do ML fitting of GLM params
-gg = cell(4, 1);
+gg = cell(k, 1);
 neglogli = 0;
 opts = {'display', 'iter', 'maxiter', 100};
-for whichn = 1:4
+for whichn = 1:k
 	%fprintf('Fitting bird #%i\n', whichn);
 	gg0 = makeFittingStruct_GLM(stas{whichn},DTsim,ggsim,whichn);  % Initialize params for fitting struct w/ sta
 	% NOTE: the call above imposes an "absref" (absolute refactory period) of 10 * DTsim. Happily this is extremely reasonable in the zf4f case.
@@ -80,7 +78,7 @@ for whichn = 1:4
 	gg0.dc = gg0.dc*0;  % Initialize to zero
 
 	gg0.tsp = tsp{whichn};   % cell spike times (vector)
-	gg0.tsp2 = tsp(setdiff(1:4, whichn));  % spike trains from "coupled" cells (cell array of vectors)
+	gg0.tsp2 = tsp(setdiff(1:k, whichn));  % spike trains from "coupled" cells (cell array of vectors)
 	gg0.tspi = 1; % 1st spike to use for computing likelihood (eg, can ignore 1st n spikes)
 	[gg{whichn}, neglogli_each] = MLfit_GLM(gg0, Stim, opts); % do ML (requires optimization toolbox)
 	printf('MLfit #%i gets neglogli %g\n', whichn, neglogli_each);
@@ -89,8 +87,8 @@ end
 
 
 %% --- Calc summary stats - used for csv and for returning ----------------------------
-for whichn = 1:4
-	for fromn = 1:4
+for whichn = 1:k
+	for fromn = 1:k
 		if whichn==fromn
 			ihdata = gg{whichn}.ih;
 		elseif whichn<fromn
@@ -120,10 +118,11 @@ if plotpath
 
 	ttk = -nkt+1:0;
 
-	for whichn = 1:4
+	for whichn = 1:k
+		% TODO num subplots should vary with k
 		subplot(2,2, whichn); % ----------------------------------
 		hold on;
-		for fromn = 1:4
+		for fromn = 1:k
 			plotcol = plotcols{fromn};
 			if whichn==fromn
 				ihdata = gg{whichn}.ih;
@@ -139,7 +138,7 @@ if plotpath
 			ylim([0, 5]);
 		end;
 		title(sprintf('Bird %i: exp(kernels) %s', whichn, runlabel));
-		legend('from 1', 'from 2', 'from 3', 'from 4', 'location', 'northeast');
+		legend('from 1', 'from 2', 'from 3', 'from 4', 'location', 'northeast'); % TODO should vary with k
 		axis tight;
 	end;
 	xlabel('time (frames)')
@@ -165,11 +164,10 @@ if csvoutpath
 	fprintf(csvfp_2d, 'runnname,frm,too,peakval,peaklag\n');
 	% data
 	fprintf(csvfp_0d, '%s,%g\n', runlabel, neglogli);
-	for whichn = 1:4
+	for whichn = 1:k
 		fprintf(csvfp_1d, '%s,%i,%i\n', runlabel, whichn, numcalls(whichn));
-		for fromn = 1:4
+		for fromn = 1:k
 			fprintf(csvfp_2d, '%s,%i,%i,%g,%g\n', runlabel, fromn, whichn, peakval(fromn,whichn), 1/peakpos(fromn,whichn));
-
 		end
 	end
 	fflush(csvfp_0d);
